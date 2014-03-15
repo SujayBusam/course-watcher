89fccbaf09723dde5f93a003ea3685d003146ec7
Sujay Busam <sujay.busam@gmail.com>
2014-03-14 21:11:01 -0400

    user can choose carrier

--- CourseWatch.py
+++ CourseWatch.py
@@ -6,16 +6,15 @@ import time
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-CLASS_YEAR = "2008"
+CLASS_YEAR = "2008" # Not sure why this is the case
 MAX_ENR = 300
 
 def main():
 	# Class data
 	url = "http://oracle-www.dartmouth.edu/dart/groucho/timetable.course_quicksearch"
-	# subject = raw_input("Enter class subject (4 letter code in lowercase): " )
-	# course_num = raw_input("Enter course number: " )
-	subject = "cosc"
-	course_num = "65"
+	subject = raw_input("Enter class subject (4 letter code in lowercase): ").lower()
+	course_num = raw_input("Enter course number: " )
+
 	post_data = {
 		"classyear": CLASS_YEAR,
 		"subj": subject,
@@ -26,10 +25,10 @@ def main():
 
 	url_data = scrape_url(url, post_data)
 
-	from_addr = 'FordPrefect064@gmail.com'
+	from_addr = raw_input("Enter email username: ")
 	pwd = raw_input("Enter email password: ")
-	to_addr  = 'sujay.busam@gmail.com'
-	phone_addr = '7245701827@mms.att.net'
+	to_addr  = from_addr
+	phone_addr = get_phone_addr()
 
 
 	while not is_class_open:
@@ -41,6 +40,21 @@ def main():
 			is_class_open = True
 		time.sleep(30)
 
+def get_phone_addr():
+	phone_num = raw_input("Enter 10 digit phone number without spaces: ")
+	carrier = raw_input("Enter your carrier (att, sprint, tmobile, verizon): ")
+
+	# Return formatted phone address based on carrier
+	if carrier.lower() == "att":
+		return phone_num + "@txt.att.net"
+	elif carrier.lower() == "sprint":
+		return phone_num + "@messaging.sprintpcs.com"
+	elif carrier.lower() == "tmobile":
+		return phone_num + "@tmomail.net"
+	elif carrier.lower() == "verizon":
+		return phone_num + "@vtext.com"
+	else:
+		return None 
 
 def scrape_url(url, post_data):
 	r = requests.post(url, data = post_data)
